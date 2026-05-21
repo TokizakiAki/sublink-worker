38964705dd1cec3f2d2d3998f557fc103c558681
TokizakiAki <130619460+TokizakiAki@users.noreply.github.com>
2026-05-21 11:47:59 +0800

    Update ruleUrls.js

--- src/config/ruleUrls.js
+++ src/config/ruleUrls.js
@@ -2,7 +2,7 @@
  * Rule Set Base URLs
  * Contains base URLs for different rule set providers
  */
-
+/**/
 export const SITE_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/sing/geo/geosite/';
 export const IP_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/sing/geo/geoip/';
 export const CLASH_SITE_RULE_SET_BASE_URL = 'https://gh-proxy.com/https://github.com/MetaCubeX/meta-rules-dat/raw/refs/heads/meta/geo/geosite/';
